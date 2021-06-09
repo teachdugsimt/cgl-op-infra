2f54133547a6e308cf676967e2a4441faa53401b
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-06-09 21:40:27 +0700

    build: s3 template 2

--- bin/main-stack.ts
+++ bin/main-stack.ts
@@ -5,6 +5,7 @@ import { MainStack } from '../lib/db-stack/nested-stack';
 import { LegacySecretsManagerStack } from '../lib/secrets-manager-stack/secrets-manager-stack';
 import { CognitoStack } from '../lib/cognito-stack/cognito-stack';
 import { DynamoDBStack } from '../lib/dynamo-db-stack/dynamo-db-stack';
+import { CargolinkDocumentStack } from '../lib/s3-cargolink-document-stack/cargolink-document-stack';
 
 const app = new cdk.App();
 
@@ -13,3 +14,4 @@ new MainStack(app, 'InfraStackDB', { env: envSgp });
 new LegacySecretsManagerStack(app, 'InfraStackSecretsHashKey', { env: envSgp });
 new CognitoStack(app, 'InfraStackCognito', { env: envSgp });
 new DynamoDBStack(app, 'InfraStackDynamoDB', { env: envSgp });
+new CargolinkDocumentStack(app, 'InfraStackS3CargolinkDocument', { env: envSgp });
