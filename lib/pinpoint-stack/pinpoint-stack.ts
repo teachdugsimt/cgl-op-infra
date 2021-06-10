45ec04fb0a474facb47fe7b615b409ec724754fe
Kittichai <king_atillart_sword3@hotmail.com>
2021-06-10 11:33:57 +0700

    feat: เพิ่ม stack pinpoint, kms, ปรับ attribute ของ cognito และ update table dynamo

---
+++ lib/pinpoint-stack/pinpoint-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from '@aws-cdk/core';
+import * as pinpoint from '@aws-cdk/aws-pinpoint';
+
+export class PinpointStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pinpointProject = new pinpoint.CfnApp(this, 'CglMessagingService', {
+      name: 'cgl-messaging-service',
+    })
+
+    const pinpointProjectSms = new pinpoint.CfnSMSChannel(this, 'CglSmsService', {
+      applicationId: pinpointProject.ref
+    })
+
+    // const pinpointProjectEmail = new pinpoint.CfnEmailChannel(this, 'CglEmailService', {
+    //   applicationId: pinpointProject.ref,
+    //   fromAddress: '',
+    //   identity: '',
+    // })
+
+  }
+}
